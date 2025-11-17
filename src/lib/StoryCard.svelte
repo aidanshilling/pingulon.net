5890a6ce9ed0f30638921821a7c4bbfdfc7885e5
aidanshilling <aidanshilling91@gmail.com>
2025-11-17 10:51:40 +0000

    Add some basic styling for the reading layout

---
+++ src/lib/StoryCard.svelte
@@ -0,0 +1,21 @@
+<script>
+	let { href, name, idx } = $props();
+</script>
+
+{#if idx > 0}
+	<div style="border-top: 1px solid black">
+		<a {href}>{name}</a>
+	</div>
+{:else}
+	<div>
+		<a {href}>{name}</a>
+	</div>
+{/if}
+
+<style>
+	div {
+		padding: 10px;
+		height: 25px;
+		width: 225px;
+	}
+</style>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import favicon from "$lib/assets/favicon.svg";
+	import StoryCard from "$lib/StoryCard.svelte";
 	import type { LayoutProps } from "./$types";
 
 	let { data, children }: LayoutProps = $props();
@@ -15,11 +16,18 @@
 
 <div id="layout">
 	<div id="nav">
-		{#each data.stories as story}
-			<a href="/stories/{story.name}">{story.name}</a>
-		{/each}
+		<div id="nav-title">Stories</div>
+		<div id="nav-container">
+			{#each data.stories as story, idx}
+				<StoryCard
+					href="/stories/{story.name}"
+					name={story.name}
+					{idx}
+				></StoryCard>
+			{/each}
+		</div>
 	</div>
-	<div>
+	<div id="content">
 		{@render children()}
 	</div>
 </div>
@@ -33,10 +41,39 @@
 	div#top-bar {
 		display: flex;
 		flex-direction: row;
+		margin-left: 8px;
+		margin-right: 8px;
+		margin-bottom: 10px;
 	}
 
 	div#nav {
+		margin: 5px;
+	}
+
+	div#nav-title {
+		border-radius: 5px;
+		border: 1px solid black;
+		margin-bottom: 5px;
+		padding: 8px;
+		font-weight: bold;
+		box-shadow: 0px 1px;
+	}
+
+	div#nav-container {
 		display: flex;
 		flex-direction: column;
+		border-radius: 5px;
+		border: 1px solid black;
+		justify-content: start;
+		height: fit-content;
+		box-shadow: 0px 1px;
+	}
+
+	div#content {
+		margin: 5px;
+		border-radius: 5px;
+		padding: 10px;
+		border: 1px solid black;
+		box-shadow: 0px 1px;
 	}
 </style>
