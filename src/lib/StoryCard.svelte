4eb4d3d95012566610da0a75142d6625dba61054
aidanshilling <aidanshilling91@gmail.com>
2025-11-18 02:14:10 +0000

    Improve mobile layout

--- src/lib/StoryCard.svelte
+++ src/lib/StoryCard.svelte
@@ -3,25 +3,30 @@
 </script>
 
 <a class:non-top-card={idx > 0} class:selected {href}>
+	{selected ? `>` : ``}
 	{name}
 </a>
 
 <style>
 	a {
 		all: unset;
-		display: block;
-		align-items: center;
-		justify-content: center;
+		width: 94%;
 		padding: 10px;
+		min-width: 300px;
 		height: 25px;
-		width: 225px;
 		background-color: rgba(0, 0, 0, 0);
 		cursor: pointer;
+	}
+	a:hover {
+		text-decoration: underline;
+		font-weight: bold;
+		/* background-color: rgba(100, 0, 00, 0.1); */
 	}
 	.non-top-card {
 		border-top: 1px solid black;
 	}
 	.selected {
-		background-color: rgba(225, 0, 0, 0.1);
+		/* background-color: rgba(225, 0, 0, 0.1); */
+		font-weight: bold;
 	}
 </style>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -16,6 +16,7 @@
 	});
 
 	let { data, children }: LayoutProps = $props();
+	let count: Number = $state(data.stories.length);
 </script>
 
 <svelte:head>
@@ -28,7 +29,10 @@
 
 <div id="layout">
 	<div id="nav">
-		<div id="nav-title">Stories</div>
+		<div id="nav-title">
+			<div>Stories</div>
+			<div>({count})</div>
+		</div>
 		<div id="nav-container">
 			{#each data.stories as story, idx}
 				<StoryCard
@@ -89,12 +93,15 @@
 	}
 
 	div#nav-title {
+		display: flex;
+		flex-direction: row;
+		justify-content: space-between;
 		border-radius: 10px;
 		border: 2px solid black;
 		margin-bottom: 5px;
 		padding: 8px;
 		font-weight: bold;
-		box-shadow: 0px 2px;
+		box-shadow: 1px 2px;
 	}
 
 	a {
@@ -109,7 +116,7 @@
 		border: 2px solid black;
 		justify-content: start;
 		height: fit-content;
-		box-shadow: 0px 2px;
+		box-shadow: 1px 2px;
 		overflow: auto;
 	}
 
@@ -120,7 +127,35 @@
 		border-radius: 10px;
 		padding: 10px;
 		border: 2px solid black;
-		box-shadow: 0px 2px;
+		box-shadow: 1px 2px;
 		overflow: auto;
+	}
+
+	@media (max-width: 600px) {
+		:global(html) {
+			width: 100vw;
+			height: 100vh;
+		}
+		:global(body) {
+			all: unset;
+			font-size: 8pt;
+			width: 100vw;
+			height: 100vh;
+			background-size: cover;
+		}
+		div#layout {
+			display: flex;
+			flex-direction: column;
+			max-height: 90vh;
+			min-width: 100vw;
+			max-width: 100vw;
+		}
+		div#content {
+			width: auto;
+		}
+		div#nav {
+			max-height: 20vh;
+			width: auto;
+		}
 	}
 </style>
